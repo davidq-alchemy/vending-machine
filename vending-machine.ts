b99593594d74b92f4cad842bc13c31dd25f23208
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-11-08 10:59:37 -0800

    Add command line argument parsing

--- vending-machine.ts
+++ vending-machine.ts
@@ -1,1 +1,38 @@
-// Implement your vending machine here!
+import process from 'node:process';
+
+/*
+ * Parse command line arguments. The script requires two arguments:
+ *     --item-cost <item_cost>
+ *     --payment <payment_amount>
+ *
+ *  Both <item_cost> and <payment_amount> are expected to be in dollar amounts.
+ */
+
+let cost: number | undefined;
+let payment: number | undefined;
+
+let arg: string | undefined;
+while ((arg = process.argv.shift()) !== undefined) {
+  switch (arg) {
+    case '--item-cost': {
+      const costInput = process.argv.shift();
+      cost = parseInt(costInput || '0');
+      break;
+    }
+    case '--payment': {
+      const paymentInput = process.argv.shift();
+      payment = parseInt(paymentInput || '0');
+      break;
+    }
+  }
+}
+
+if (!cost || !payment) {
+  // FIXME: I don't think this is conforms to the POSIX spec that was suggested.
+  console.log('Usage:');
+  console.log('  vending-machine --item-cost <item_cost> --payment <payment_amount>')
+  console.log('Options:');
+  console.log('  --item-cost: cost of item in dollars');
+  console.log('  --payment:   payment amount in dollars');
+  process.exit(1);
+}
