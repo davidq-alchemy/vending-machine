9166eff0210ced27701068718784a9651c7b97a7
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-11-08 12:33:16 -0800

    Add ESLint support to project

--- vending-machine.ts
+++ vending-machine.ts
@@ -15,35 +15,35 @@ let help = false;
 let arg: string | undefined;
 while ((arg = process.argv.shift()) !== undefined) {
   switch (arg) {
-    case '--item-cost': {
-      const costInput = process.argv.shift();
-      costInDollars = Number(costInput);
-      if (!costInDollars) {
-        console.error(`Unable to parse '${costInput}' as item_cost.`);
-        process.exit(1);
-      }
-      break;
+  case '--item-cost': {
+    const costInput = process.argv.shift();
+    costInDollars = Number(costInput);
+    if (!costInDollars) {
+      console.error(`Unable to parse '${costInput}' as item_cost.`);
+      process.exit(1);
     }
-    case '--payment': {
-      const paymentInput = process.argv.shift();
-      paymentInDollars = Number(paymentInput);
-      if (!paymentInDollars) {
-        console.error(`Unable to parse '${paymentInput}' as payment_amount.`);
-        process.exit(1);
-      }
-      break;
+    break;
+  }
+  case '--payment': {
+    const paymentInput = process.argv.shift();
+    paymentInDollars = Number(paymentInput);
+    if (!paymentInDollars) {
+      console.error(`Unable to parse '${paymentInput}' as payment_amount.`);
+      process.exit(1);
     }
-    case '--help':
-    case '-h':
-      help = true;
-      break;
+    break;
+  }
+  case '--help':
+  case '-h':
+    help = true;
+    break;
   }
 }
 
 if (!costInDollars || !paymentInDollars || help) {
   // FIXME: I don't think this conforms to the POSIX spec that was suggested.
   console.log('Usage:');
-  console.log('  vending-machine --item-cost <item_cost> --payment <payment_amount>')
+  console.log('  vending-machine --item-cost <item_cost> --payment <payment_amount>');
   console.log('Options:');
   console.log('  --item-cost: cost of item in dollars');
   console.log('  --payment:   payment amount in dollars');
