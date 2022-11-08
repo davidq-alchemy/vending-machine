05c4f633400bde4f7d05d277ef9aa53c69f21fd6
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-11-08 15:29:09 -0800

    Refactor some logic into functions

--- vending-machine.ts
+++ vending-machine.ts
@@ -1,97 +1,91 @@
 import process from 'node:process';
 import fs from 'node:fs';
 
+// Types
 type Coin = {
   name: string,
   value: number,
 }
-
 type Currency = {
   abbreviation: string,
   coins: ReadonlyArray<Coin>,
   format: string,
 }
+type VendingMachineOptions = {
+  currency: Currency,
+  cost: number,
+  payment: number,
+}
+type ChangeInstructions = ReadonlyArray<{ coin: Coin, count: number }>;
 
-/*
- * Load currencies file. './currencies.json' contains an array of currency definitions for supported currencies.
- */
-
+// Helper functions
 function loadCurrencies(path: string): ReadonlyArray<Currency> {
   const data = JSON.parse(fs.readFileSync(path, { encoding: 'utf-8' }));
   // FIXME: It would be nice to make this type safe.
   return data as ReadonlyArray<Currency>;
 }
 
-const currencies = loadCurrencies('./currencies.json');
+function parseArgs(currencies: ReadonlyArray<Currency>): VendingMachineOptions {
+  let currencyCode: string | undefined = 'USD';
+  let costInDollars: number | undefined;
+  let paymentInDollars: number | undefined;
+  let help = false;
 
-/*
- * Parse command line arguments. The script requires two arguments:
- *     --item-cost <item_cost>
- *     --payment <payment_amount>
- *
- *  Both <item_cost> and <payment_amount> are expected to be in dollar amounts.
- */
+  let arg: string | undefined;
+  while ((arg = process.argv.shift()) !== undefined) {
+    switch (arg) {
+    case '--item-cost': {
+      const costInput = process.argv.shift();
+      costInDollars = Number(costInput);
+      if (!costInDollars) {
+        console.error(`Unable to parse '${costInput}' as item_cost.`);
+        process.exit(1);
+      }
+      break;
+    }
+    case '--payment': {
+      const paymentInput = process.argv.shift();
+      paymentInDollars = Number(paymentInput);
+      if (!paymentInDollars) {
+        console.error(`Unable to parse '${paymentInput}' as payment_amount.`);
+        process.exit(1);
+      }
+      break;
+    }
+    case '--currency': {
+      currencyCode = process.argv.shift();
+      if (!currencyCode) {
+        console.error(`Unable to parse '${currencyCode}' as currency_code`);
+        process.exit(1);
+      }
+      break;
+    }
+    case '--help':
+    case '-h':
+      help = true;
+      break;
+    }
+  }
 
-let currencyCode: string | undefined = 'USD';
-let costInDollars: number | undefined;
-let paymentInDollars: number | undefined;
-let help = false;
+  const currency = currencies.find(c => c.abbreviation === currencyCode);
 
-let arg: string | undefined;
-while ((arg = process.argv.shift()) !== undefined) {
-  switch (arg) {
-  case '--item-cost': {
-    const costInput = process.argv.shift();
-    costInDollars = Number(costInput);
-    if (!costInDollars) {
-      console.error(`Unable to parse '${costInput}' as item_cost.`);
-      process.exit(1);
-    }
-    break;
+  if (!costInDollars || !paymentInDollars || !currency || help) {
+    // FIXME: I don't think this conforms to the POSIX spec that was suggested.
+    console.log('Usage:');
+    console.log('  vending-machine --item-cost <item_cost> --payment <payment_amount> [--currency <currency_code>]');
+    console.log('Options:');
+    console.log('  --item-cost: cost of item in dollars');
+    console.log('  --payment:   payment amount in dollars');
+    console.log('  --currency:  currency ISO 4217 code');
+    process.exit(1);
   }
-  case '--payment': {
-    const paymentInput = process.argv.shift();
-    paymentInDollars = Number(paymentInput);
-    if (!paymentInDollars) {
-      console.error(`Unable to parse '${paymentInput}' as payment_amount.`);
-      process.exit(1);
-    }
-    break;
-  }
-  case '--currency': {
-    currencyCode = process.argv.shift();
-    if (!currencyCode) {
-      console.error(`Unable to parse '${currencyCode}' as currency_code`);
-      process.exit(1);
-    }
-    break;
-  }
-  case '--help':
-  case '-h':
-    help = true;
-    break;
-  }
+
+  return {
+    currency,
+    cost: Math.floor(costInDollars * 100),
+    payment: Math.floor(paymentInDollars * 100)
+  };
 }
-
-const currency = currencies.find(c => c.abbreviation === currencyCode);
-
-if (!costInDollars || !paymentInDollars || !currency || help) {
-  // FIXME: I don't think this conforms to the POSIX spec that was suggested.
-  console.log('Usage:');
-  console.log('  vending-machine --item-cost <item_cost> --payment <payment_amount>');
-  console.log('Options:');
-  console.log('  --item-cost: cost of item in dollars');
-  console.log('  --payment:   payment amount in dollars');
-  process.exit(1);
-}
-
-/*
- *  Main program logic.
- *
- *  From here on all currency amounts should be in pennies.
- */
-
-type ChangeInstructions = ReadonlyArray<{ coin: Coin, count: number }>;
 
 function makeChange(amount: number, currency: Currency): ChangeInstructions {
   // NOTE: These coins start in sorted descending order.
@@ -112,8 +106,17 @@ function makeChange(amount: number, currency: Currency): ChangeInstructions {
   return changeInstructions;
 }
 
-const cost = Math.floor(costInDollars * 100);
-const payment = Math.floor(paymentInDollars * 100);
+function printChangeInstructions(instructions: ChangeInstructions): void {
+  const longestCoinName = instructions.reduce((max, step) => Math.max(step.coin.name.length, max), Number.MIN_VALUE);
+  for (const step of instructions) {
+    console.log(`${(step.coin.name + ':').padEnd(longestCoinName + 1)} ${step.count}`);
+  }
+  console.log(`Total Change: ${currency.format.replace('<AMOUNT>', (change / 100).toString())}`);
+}
+
+// Main program logic
+const currencies = loadCurrencies('./currencies.json');
+const { currency, cost, payment } = parseArgs(currencies);
 const change = payment - cost;
 
 if (change < 0) {
@@ -122,9 +125,4 @@ if (change < 0) {
 }
 
 const changeInstructions = makeChange(change, currency);
-
-const longestCoinName = changeInstructions.reduce((max, step) => Math.max(step.coin.name.length, max), Number.MIN_VALUE);
-for (const step of changeInstructions) {
-  console.log(`${(step.coin.name + ':').padEnd(longestCoinName + 1)} ${step.count}`);
-}
-console.log(`Total Change: ${currency.format.replace('<AMOUNT>', (change / 100).toString())}`);
+printChangeInstructions(changeInstructions);
